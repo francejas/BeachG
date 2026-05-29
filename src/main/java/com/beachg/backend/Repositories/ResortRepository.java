a8948e6af41f38d23ce99fcb2d82c9dddf92f0f7
AxelOvejeroGallardo <124913653+AxelOvejeroGallardo@users.noreply.github.com>
2026-05-29 09:34:41 -0300

    hice la clase Resort, el enumStatus, la interfaz ResortRepository, la clase ResortService, y el ResortController

---
+++ src/main/java/com/beachg/backend/Repositories/ResortRepository.java
@@ -0,0 +1,9 @@
+package com.beachg.backend.Repositories;
+
+
+import com.beachg.backend.Models.Resort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResortRepository extends JpaRepository<Resort,Long> {
+}
+
