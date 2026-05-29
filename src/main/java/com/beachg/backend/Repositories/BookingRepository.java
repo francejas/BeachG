641643673295e0fd7b28b5e71dca44cd223a8e99
Francisco Cejas <138420593+francejas@users.noreply.github.com>
2026-05-29 15:38:38 -0300

    Delete src/main/java/com/beachg/backend/Repositories directory

--- src/main/java/com/beachg/backend/Repositories/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.beachg.backend.repositories;
-
-import com.beachg.backend.Models.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
