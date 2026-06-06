26e34b6c02ad23e06a256c2c210b9c6cc2d71528
FacuGauthier <facundosgauthier@gmail.com>
2026-06-06 00:05:24 -0300

    add necessary imports to BookingService

--- src/main/java/com/beachg/backend/services/BookingService.java
+++ src/main/java/com/beachg/backend/services/BookingService.java
@@ -1,5 +1,9 @@
 package com.beachg.backend.services;
 
+import com.beachg.backend.repositories.BookingRepository;
+import com.beachg.backend.repositories.ClientRepository;
+import com.beachg.backend.repositories.GuestRepository;
+import com.beachg.backend.repositories.RentalUnitRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
