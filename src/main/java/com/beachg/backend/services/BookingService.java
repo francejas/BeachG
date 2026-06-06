21ec31b9791bcb57c58f017530eba7c8588caa5b
FacuGauthier <facundosgauthier@gmail.com>
2026-06-05 23:25:47 -0300

    add BookingService & BookingControllers voids

---
+++ src/main/java/com/beachg/backend/services/BookingService.java
@@ -0,0 +1,9 @@
+package com.beachg.backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookingService {
+}
