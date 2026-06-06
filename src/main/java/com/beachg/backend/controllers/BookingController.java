ea8bf6b09e569c2c56b27b7d3449e33b50ddc09b
FacuGauthier <facundosgauthier@gmail.com>
2026-06-05 23:38:06 -0300

    fix name of url on RequestMapping

--- src/main/java/com/beachg/backend/controllers/BookingController.java
+++ src/main/java/com/beachg/backend/controllers/BookingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/apu/bookings")
+@RequestMapping("/api/bookings")
 @CrossOrigin("*")
 @RequiredArgsConstructor
 public class BookingController {
