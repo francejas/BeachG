82bcc3cb669ab18ec0ce2dca2484f3ced31520c6
FacuGauthier <facundosgauthier@gmail.com>
2026-06-12 01:08:33 -0300

    add news imports of java for SecretKey & more

--- src/main/java/com/beachg/backend/security/JwtUtil.java
+++ src/main/java/com/beachg/backend/security/JwtUtil.java
@@ -6,6 +6,8 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
